90a81f86b14612be5e6094c220574f88b9a9cd76
Bezrukov Andrey <andrbezr2016@yandex.ru>
2025-05-02 17:35:00 +0300

    add clients logic

--- src/main/kotlin/com/andrbezr2016/library/ui/service/CatalogLoaderService.kt
+++ src/main/kotlin/com/andrbezr2016/library/ui/service/CatalogLoaderService.kt
@@ -2,15 +2,40 @@ package com.andrbezr2016.library.ui.service
 
 import com.andrbezr2016.library.ui.dto.BookDto
 import com.andrbezr2016.library.ui.dto.BookFilter
+import com.andrbezr2016.library.ui.dto.BookInput
+import com.andrbezr2016.library.ui.dto.BookUpdate
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.graphql.client.GraphQlClient
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class CatalogLoaderService(val graphQlClient: GraphQlClient) {
 
     fun getBooks(bookFilter: BookFilter?): Collection<BookDto?>? {
         return graphQlClient.documentName("getBooks").variable("bookFilter", bookFilter).retrieveSync("getBooks")
+            .toEntity<MutableCollection<BookDto?>?>(object :
+                ParameterizedTypeReference<MutableCollection<BookDto?>?>() {
+            })
+    }
+
+    fun addBook(bookInput: BookInput?): Collection<BookDto?>? {
+        return graphQlClient.documentName("addBook").variable("bookInput", bookInput).retrieveSync("addBook")
+            .toEntity<MutableCollection<BookDto?>?>(object :
+                ParameterizedTypeReference<MutableCollection<BookDto?>?>() {
+            })
+    }
+
+    fun updateBook(bookId: UUID?, bookUpdate: BookUpdate?): Collection<BookDto?>? {
+        return graphQlClient.documentName("updateBook").variables(mapOf("id" to bookId, "bookUpdate" to bookUpdate))
+            .retrieveSync("updateBook")
+            .toEntity<MutableCollection<BookDto?>?>(object :
+                ParameterizedTypeReference<MutableCollection<BookDto?>?>() {
+            })
+    }
+
+    fun deleteBook(bookId: UUID?): Collection<BookDto?>? {
+        return graphQlClient.documentName("deleteBook").variable("id", bookId).retrieveSync("deleteBook")
             .toEntity<MutableCollection<BookDto?>?>(object :
                 ParameterizedTypeReference<MutableCollection<BookDto?>?>() {
             })
--- src/main/kotlin/com/andrbezr2016/library/ui/service/RecommendationLoaderService.kt
+++ src/main/kotlin/com/andrbezr2016/library/ui/service/RecommendationLoaderService.kt
@@ -1,8 +1,79 @@
 package com.andrbezr2016.library.ui.service
 
+import com.andrbezr2016.library.ui.dto.*
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.util.*
 
 @Service
 class RecommendationLoaderService(val webClient: WebClient) {
+
+    fun addNotableBook(notableBookInput: NotableBookInput?): NotableBookDto {
+        return webClient.post()
+            .uri("/notableBooks/add")
+            .body(BodyInserters.fromValue(notableBookInput!!))
+            .retrieve()
+            .bodyToMono<NotableBookDto>().blockOptional().orElseThrow()
+    }
+
+    fun updateNotableBook(notableBookId: UUID?, notableBookUpdate: NotableBookUpdate?): NotableBookDto {
+        return webClient.patch()
+            .uri("/notableBooks/{notableBookId}/update", notableBookId)
+            .body(BodyInserters.fromValue(notableBookUpdate!!))
+            .retrieve()
+            .bodyToMono<NotableBookDto>().blockOptional().orElseThrow()
+    }
+
+    fun deleteNotableBook(notableBookId: UUID?): NotableBookDto {
+        return webClient.delete()
+            .uri("/notableBooks/{notableBookId}/delete", notableBookId)
+            .retrieve()
+            .bodyToMono<NotableBookDto>().blockOptional().orElseThrow()
+    }
+
+    fun getNotableBook(notableBookId: UUID?): NotableBookDto {
+        return webClient.get()
+            .uri("/notableBooks/{notableBookId}", notableBookId)
+            .retrieve()
+            .bodyToMono<NotableBookDto>().blockOptional().orElseThrow()
+    }
+
+    fun getNotableBooks(): MutableCollection<NotableBookDto?> {
+        return webClient.get()
+            .uri("/notableBooks/all")
+            .retrieve()
+            .bodyToMono<MutableCollection<NotableBookDto?>>().blockOptional().orElseThrow()
+    }
+
+    fun addNote(notableBookId: UUID?, noteInput: NoteInput?): NoteDto {
+        return webClient.post()
+            .uri("/notableBooks/{notableBookId}/notes/add", notableBookId)
+            .body(BodyInserters.fromValue(noteInput!!))
+            .retrieve()
+            .bodyToMono<NoteDto>().blockOptional().orElseThrow()
+    }
+
+    fun updateNote(noteId: UUID?, noteUpdate: NoteUpdate?): NoteDto {
+        return webClient.patch()
+            .uri("/notableBooks/notes/{noteId}/update", noteId)
+            .body(BodyInserters.fromValue(noteUpdate!!))
+            .retrieve()
+            .bodyToMono<NoteDto>().blockOptional().orElseThrow()
+    }
+
+    fun deleteNote(noteId: UUID?): NoteDto {
+        return webClient.delete()
+            .uri("/notableBooks/notes/{noteId}/delete", noteId)
+            .retrieve()
+            .bodyToMono<NoteDto>().blockOptional().orElseThrow()
+    }
+
+    fun getBooksToRead(): MutableCollection<BookDto?> {
+        return webClient.get()
+            .uri("/books/getBooksToRead")
+            .retrieve()
+            .bodyToMono<MutableCollection<BookDto?>>().blockOptional().orElseThrow()
+    }
 }
