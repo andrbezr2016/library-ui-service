17565883f22bdcc5ec621f61a8cf6a9622041295
Bezrukov Andrey <andrbezr2016@yandex.ru>
2025-05-05 19:15:19 +0300

    update controller

--- src/main/kotlin/com/andrbezr2016/library/ui/controller/LibraryController.kt
+++ src/main/kotlin/com/andrbezr2016/library/ui/controller/LibraryController.kt
@@ -1,13 +1,58 @@
 package com.andrbezr2016.library.ui.controller
 
+import com.andrbezr2016.library.ui.dto.BookDto
+import com.andrbezr2016.library.ui.dto.BookInput
+import com.andrbezr2016.library.ui.dto.BookUpdate
+import com.andrbezr2016.library.ui.dto.NotableBookDto
 import com.andrbezr2016.library.ui.service.LibraryService
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
+
 
 @Controller
 class LibraryController(val libraryService: LibraryService) {
 
-    @GetMapping("/index")
-    fun index(model: Model) = "index"
+    @RequestMapping(value = ["/", "/index"])
+    fun index(): String {
+        return "index"
+    }
+
+    @RequestMapping(value = ["/index"], params = ["addBook"])
+    fun addBook(bookInput: BookInput, model: ModelMap): String {
+        libraryService.addBook(bookInput)
+        model.clear()
+        return "redirect:/index"
+    }
+
+    @RequestMapping(value = ["/index"], params = ["updateBook"])
+    fun updateBook(bookId: UUID, bookUpdate: BookUpdate, model: ModelMap): String {
+        libraryService.updateBook(bookId, bookUpdate)
+        model.clear()
+        return "redirect:/index"
+    }
+
+    @RequestMapping(value = ["/index"], params = ["deleteBook"])
+    fun deleteBook(bookId: UUID, model: ModelMap): String {
+        libraryService.deleteBook(bookId)
+        model.clear()
+        return "redirect:/index"
+    }
+
+    @ModelAttribute("allBooks")
+    fun getBooks(): Collection<BookDto?>? {
+        return libraryService.getBooks()
+    }
+
+    @ModelAttribute("allBooksToRead")
+    fun getBooksToRead(): Collection<BookDto?>? {
+        return libraryService.getBooksToRead()
+    }
+
+    @ModelAttribute("allNotableBooks")
+    fun getNotableBooks(): Collection<NotableBookDto?>? {
+        return libraryService.getNotableBooks()
+    }
 }
--- src/main/kotlin/com/andrbezr2016/library/ui/service/LibraryService.kt
+++ src/main/kotlin/com/andrbezr2016/library/ui/service/LibraryService.kt
@@ -1,10 +1,36 @@
 package com.andrbezr2016.library.ui.service
 
+import com.andrbezr2016.library.ui.dto.*
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class LibraryService(
     val catalogLoaderService: CatalogLoaderService,
     val recommendationLoaderService: RecommendationLoaderService
 ) {
+
+    fun getBooks(): Collection<BookDto?>? {
+        return catalogLoaderService.getBooks(BookFilter())
+    }
+
+    fun getBooksToRead(): Collection<BookDto?>? {
+        return recommendationLoaderService.getBooksToRead()
+    }
+
+    fun getNotableBooks(): Collection<NotableBookDto?>? {
+        return recommendationLoaderService.getNotableBooks()
+    }
+
+    fun addBook(bookInput: BookInput) {
+        catalogLoaderService.addBook(bookInput)
+    }
+
+    fun updateBook(bookId: UUID, bookUpdate: BookUpdate) {
+        catalogLoaderService.updateBook(bookId, bookUpdate)
+    }
+
+    fun deleteBook(bookId: UUID) {
+        catalogLoaderService.deleteBook(bookId)
+    }
 }
